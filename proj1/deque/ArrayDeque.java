28e0f46936d0e02f6516c9b520896e724e6afc91
1591697 <116343855+1591697@users.noreply.github.com>
2024-02-27 19:25:30 +0800

    proj1 first

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,31 +1,83 @@
 package deque;
 
+
 import java.util.Iterator;
 
 public class ArrayDeque <T> implements Deque<T>{
-    private T[] arr;
+     private T[] arr;
     private  int head;
     private  int tail;
-    private  int size=0;
-    @Override
-    public void addFirst(T item){
-        if(tail!=head){
-            if(head==0) {
-                arr[head] = item;
-                head = arr.length;
-                size++;
+    private  int size;
+    public  ArrayDeque(){
+        arr=(T[])new Object[8];
+        head=0;
+        tail=0;
+        size=0;
+    }
+    public boolean isfull(){
+        if(size>=arr.length)
+        return true;
+        return false;
+    }
+    public  void resize(){
+
+            T[] a=(T[])new Object[arr.length*2];
+//            for(int i=0;i<arr.length;i++){
+//                if(head+i<arr.length)
+//                a[i]=arr[head+i];
+//                if(head+i>=arr.length){
+//                    a[i]=a[i-arr.length+head];
+//                }
+//            }
+            for(int i=0;i<tail;i++){
+                a[i]=arr[i];
             }
-            else{
+            for(int i=head+1 ;i<arr.length;i++){
+                a[i+arr.length]=arr[i];
+            }
+            head=arr.length+head;
+
+            arr=a;
+        }
+
+
+    @Override
+    public void addFirst(T item) {
+        if (!isfull()) {
+            if(head==0||tail==0){
+                tail=1;
+            }
+            if (head == 0) {
+                arr[head] = item;
+                head = arr.length - 1;
+                size++;
+            } else {
                 arr[head] = item;
                 head--;
                 size++;
             }
         }
-
+        else{
+            resize();
+            if (head == 0) {
+                arr[head] = item;
+                head = arr.length - 1;
+                size++;
+            } else {
+                arr[head] = item;
+                head--;
+                size++;
+            }
+        }
     }
+
+
     public void addLast(T item){
-        if(tail!=head){
-            if(tail==arr.length) {
+        if(size<arr.length-2){
+            if(head==0||tail==0){
+                head=arr.length-1;
+            }
+            if(tail==arr.length-1) {
                 arr[tail] = item;
                 tail = 0;
                 size++;
@@ -36,18 +88,34 @@ public class ArrayDeque <T> implements Deque<T>{
                 size++;
             }
         }
+        else{
+            resize();
+            if(tail==arr.length-1) {
+                arr[tail] = item;
+                tail = 0;
+                size++;
+            }
+            else{
+                arr[tail] = item;
+                tail++;
+                size++;
+            }
+
+        }
 
     }
     public boolean isEmpty(){
+        if (size == 0) {
 
-        return head==tail;
-
+            return true;
+        }
+return false;
     }
     public int size(){
         return  size;
     }
     public void printDeque(){
-        int num=0;
+        int num;
         if(head>tail) {
 
             for(int i=head;i<arr.length;i++){
@@ -67,50 +135,134 @@ public class ArrayDeque <T> implements Deque<T>{
 
     }
     public T removeFirst(){
-        T n=arr[head];
-        arr[head]=null;
-        if(head==0){
-            head=arr.length;
-            size--;
-        }
-        else {
-            head++;
-            size--;
-        }
-        return  n;
-    }
-    public T removeLast(){
-        T n=arr[tail-1];
-        arr[tail-1]=null;
-        if(tail==arr.length){
-            tail=0;
-            size--;
-        }
-        else {
-            tail--;
-            size--;
-        }
-        return  n;
+        if(!isEmpty()) {
 
-    }
-    public T get(int index){
-        if(arr[index]!=null)
-            return  arr[index];
+            if (head ==arr.length-1) {
+                T n = arr[0];
+    arr[0]=null;
+
+                head = 0;
+                size--;
+                return n;
+            } else {
+                T n=arr[head+1];
+                arr[head+1] = null;
+
+                head++;
+                size--;
+                return n;
+            }
+
+
+        }
         return null;
     }
-    public Iterator<T> iterator(){
-        return  null;
+    public T removeLast(){
+        if(!isEmpty()) {
+            T n;
+            if(tail-1>=0) {
+                 n = arr[tail - 1];
+                arr[tail - 1] = null;
+            }
+            else{
+                 n = arr[arr.length-1];
+                arr[arr.length-1] = null;
+            }
+            if (tail == arr.length) {
+                tail = 0;
+                size--;
+            } else {
+                tail--;
+                size--;
+            }
+            return n;
+        }
+        return null;
     }
+
+
+    public T get(int index){
+
+if(index>=size){
+    return null;
+}
+else{
+    if(head-index<0){
+        return arr[arr.length-index+head];
+    }
+    else {
+        return arr[head-index];
+    }
+}
+//else{
+//    if(head>tail) {
+//    if(index<arr.length-head) {
+//    return arr[head+index];
+//}
+//        else{
+//            return arr[index-(arr.length-head)];
+//        }
+//    }
+//    else{
+//        if(index==0){
+//            return arr[head];
+//        }
+//        else {
+//            return arr[head + index];
+//        }
+//    }
+//
+//}
+//
+//
+   }
+    public Iterator<T> iterator() {
+        return new ArrayDequeIterator();
+    }
+    public class ArrayDequeIterator implements Iterator<T>{
+        private int wizPos;
+
+        private ArrayDequeIterator() {
+            wizPos = 0;
+        }
+        public boolean hasNext(){
+return size<wizPos;
+        }
+        public T next(){
+            T item = get(wizPos);
+            wizPos += 1;
+            return item;
+        }
+
+    }
+    //equals 的写法。
     public boolean equals(Object o){
-
-        return  false;
+        if(this==o){
+            return true;
+        }
+        if(o==null){
+            return false;
+        }
+        if(!(o instanceof Deque)){
+            return false;
+        }
+        Deque<T> oa=(Deque<T>) o;
+        if(this.size()!=oa.size()){
+            return false;    }
+        for(int i=0;i<oa.size();i++){
+            if(oa.get(i)!=this.get(i)){
+                return false;
+            }
+        }
+        return true;
     }
+    public static void main(String[] args) {
+        ArrayDeque<Integer> a=new ArrayDeque<>();
+        a.addFirst(1);
+      a.removeFirst();
+        a.printDeque();
 
-    public ArrayDeque(){
-        arr=(T[]) new Object[10];
-        head=0;
-        tail=0;
+   }
 
-    }
 
 }
--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -3,117 +3,179 @@ package deque;
 import java.util.Iterator;
 
 public  class  LinkedListDeque<T>implements Deque<T> {
-
-    private T[] arr;
-    private  int head;
-    private  int tail;
-    private  int size=0;
+    class  node{ private T item;
+        private   node pre;
+        private   node next;
+       node(node pre,node next,T item){
+           this.pre=pre;
+           this.next=next;
+           this.item=item;
+       }
+    }
+    private int size;
+    private node sen;
     @Override
     public void addFirst(T item){
-        if(tail!=head){
-            if(head==0) {
-                arr[head] = item;
-                head = arr.length;
-                size++;
-            }
-            else{
-                arr[head] = item;
-                head--;
-                size++;
-            }
-        }
+    size++;
+    node k=new node(null,null,item);
+    k.next=sen.next;
+    sen.next.pre=k;
+    sen.next=k;
+    k.pre=sen;
 
     }
     public void addLast(T item){
-        if(tail!=head){
-            if(tail==arr.length) {
-                arr[tail] = item;
-                tail = 0;
-                size++;
-            }
-            else{
-                arr[tail] = item;
-                tail++;
-                size++;
-            }
-        }
+        size++;
+        node k=new node(null,null,item);
+
+
+            k.pre=sen.pre;
+            sen.pre.next=k;
+        k.next=sen;
+        sen.pre=k;
 
     }
     public boolean isEmpty(){
+    if(sen.next==sen||sen.pre==sen){
+        return true;
+    }
+    return false;
 
-        return head==tail;
 
     }
     public int size(){
-        return  size;
+return size;
     }
     public void printDeque(){
-        int num=0;
-        if(head>tail) {
+    node k=new node(null,null,null);
+        k=sen;
+    if(size==0){
+        System.out.println("null");
+    }
+    for(int i=0;i<size;i++){
 
-            for(int i=head;i<arr.length;i++){
-                System.out.println((arr[i]));
-            }
-            for(int i=0;i<tail;i++){
-                System.out.println(arr[i]);
-            }
-        }
-        else{
-             num=tail-head;
-            for(int i=0;i<num;i++){
-                System.out.println(arr[head+i]);
-            }
-        }
+        System.out.println(k.next.item);
+        k=k.next;
+    }
 
 
     }
     public T removeFirst(){
-        T n=arr[head];
-        arr[head]=null;
-        if(head==0){
-            head=arr.length;
-            size--;
-        }
-        else {
-            head++;
-            size--;
-        }
-        return  n;
+    T k;
+
+    if(size==0){
+        return null;
+    }
+
+        size--;
+        k=sen.next.item;
+        sen.next.next.pre=sen;
+        sen.next=sen.next.next;
+        return k;
+
+
     }
     public T removeLast(){
-        T n=arr[tail-1];
-        arr[tail-1]=null;
-        if(tail==arr.length){
-            tail=0;
-            size--;
+        T k;
+
+        if(size==0){
+            return null;
         }
-        else {
-            tail--;
-            size--;
-        }
-        return  n;
+
+            k=sen.pre.item;
+        size--;
+            sen.pre.pre.next=sen;
+            sen.pre=sen.pre.pre;
+            return k;
+
+
 
     }
     public T get(int index){
-        if(arr[index]!=null)
-        return  arr[index];
-        return null;
-    }
-    public Iterator<T> iterator(){
-        return  null;
-    }
-    public boolean equals(Object o){
+        node k=new node(null,null,null);
+        k=sen.next;
+        if(index>=size){
+            return null;
+        }
+        else{
 
-        return  false;
+                if(index==0){
+                    return k.item;
+                }
+                for (int i = 0; i <index; i++) {
+                    k = k.next;
+                }
+                return k.item;
+
+        }
+
     }
+//    public Iterator<T> iterator(){
+//
+//    }
+//    public boolean equals(Object o){
+//
+//
+//    }
 
     public LinkedListDeque(){
-        arr=(T[]) new Object[10];
-        head=0;
-        tail=0;
-
+    sen=new node(null,null,null);
+    sen.next=sen;
+    sen.pre=sen;
+    size=0;
     }
     public T getRecursive(int index){
-        return  null;
+        if(index>=size){
+            return null;
+        }
+
+        return  getRecursive(0,index,sen.next);
     }
+    public T getRecursive(int pos,int index,node x){
+if(pos==index){
+    return x.item;
+}
+return getRecursive(pos +1,index,x.next);
+    }
+    public Iterator<T> iterator() {
+        return new DequeIterator();
+    }
+    public class DequeIterator implements Iterator<T>{
+        private int wizPos;
+
+        private DequeIterator() {
+            wizPos = 0;
+        }
+        public boolean hasNext(){
+            return size<wizPos;
+        }
+        public T next(){
+            T item = get(wizPos);
+            wizPos += 1;
+            return item;
+        }
+
+    }
+    //equals 的写法。
+    public boolean equals(Object o){
+        if(this==o){
+            return true;
+        }
+        if(o==null){
+            return false;
+        }
+        if(!(o instanceof Deque)){
+            return false;
+        }
+        Deque<T> oa=(Deque<T>) o;
+        if(this.size()!=oa.size()){
+            return false;    }
+        for(int i=0;i<oa.size();i++){
+            if(oa.get(i)!=this.get(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }

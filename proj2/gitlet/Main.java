2cdcbb74914bea664ba0c9c5f842e94dba3fafa4
1591697 <116343855+1591697@users.noreply.github.com>
2024-04-03 11:30:45 +0800

    4/3--2

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -11,7 +11,7 @@ public class Main {
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         if(args.length==0){
             System.out.println("Please enter a command.");
             System.exit(0);
@@ -21,32 +21,52 @@ public class Main {
         switch(firstArg) {
             case "init":
                 validArgs(args, 1);
-               Repository.init();
-               break;
+                try {
+                    Repository.init();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
                 // TODO: handle the `init` command
             case "add":
                 validArgs(args, 2);
 
+                try {
                     Repository.add(args[1]);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
                 // TODO: handle the `add [filename]` command
                 break;
             case "commit":
                 validArgs(args, 2);
 
-               Repository.commit(args[1]);
+                try {
+                    Repository.commit(args[1]);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
             // TODO: FILL THE REST IN
 
             case "rm":
                 validArgs(args, 2);
 
-                Repository.rmFile(args[1]);
+                try {
+                    Repository.rmFile(args[1]);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
             case "log":
                 validArgs(args, 1);
 
-                Repository.log();
+                try {
+                    Repository.log();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
             case "global-log":
                 validArgs(args, 1);
@@ -61,7 +81,11 @@ public class Main {
             case "status":
                 validArgs(args, 1);
 
-                Repository.status();
+                try {
+                    Repository.status();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
             case "checkout":
 
@@ -72,7 +96,11 @@ public class Main {
                     }
                     if(args[1].equals("--")){
 
-                        Repository.checkout1(args[2]);
+                        try {
+                            Repository.checkout1(args[2]);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
 
                 }
@@ -82,12 +110,20 @@ public class Main {
                         System.exit(0);
                     }
                     if (args[2].equals("--")) {
-                        Repository.checkout2(args[1], args[3]);
+                        try {
+                            Repository.checkout2(args[1], args[3]);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
                else if(args.length == 2){
 
+                    try {
                         Repository.checkout3(args[1],Repository.GetBeCheckoutBids());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
 
                 }
                else{
@@ -99,7 +135,11 @@ public class Main {
                 validArgs(args, 2);
 
 
-                Repository.Creatbranch(args[1]);
+                try {
+                    Repository.Creatbranch(args[1]);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
             case "rm-branch":
                 validArgs(args, 2);
@@ -109,12 +149,20 @@ public class Main {
             case "reset":
                 validArgs(args, 2);
 
-                Repository.reset(args[1]);
+                try {
+                    Repository.reset(args[1]);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
             case "merge":
                 validArgs(args, 2);
 
-                Repository.merge(args[1]);
+                try {
+                    Repository.merge(args[1]);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
 
         }
